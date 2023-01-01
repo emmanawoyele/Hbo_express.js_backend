f9ff173436802482d9737dda96f0858e37a67fb7
emmanuel <emmanawoyele@gmail.com>
2023-01-01 07:53:30 +0000

    origin

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 5000
 // }
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:3000/create'
+  origin: '*'
 })); 
 app.use(useCreateUserRouter)
 app.use(useCommentsRouter)
