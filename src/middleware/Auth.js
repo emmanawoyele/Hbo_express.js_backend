189dfaabdf176f94cab9110ede337bce992235bf
emmanuel <emmanawoyele@gmail.com>
2023-01-13 12:39:00 +0000

    update

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -6,7 +6,7 @@ try{
     const headertoken = req.header('Authorization').replace('Bearer ','')
     console.log({headertoken})
     const verifyJwt =  jwt.verify(headertoken,process.env.JWT_ENV)
-    console.log(verifyJwt)
+    console.log({verifyJwt})
     const finduser= await createUsers.findOne({_id:verifyJwt._id , 'token.tokenid':headertoken})
     if(!finduser){
         throw new Error()
