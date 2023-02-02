a0fd4ef2ce37b135da7915866ffac02154395f54
emmanuel awoyele <emmanawoyele@gmail.com>
2023-02-02 00:26:57 +0000

    ssr

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -3,10 +3,20 @@ const jwt = require('jsonwebtoken')
 const createUsers =require('../DataBase/model/creatUser')
 const auth= async(req,res,next)=>{
     console.log({kiss:req.query})
+    
 try{
     const headertoken = req.header('Authorization').replace('Bearer ','')
-    if(!header){
-const head = req.query
+    if(!req.header){
+const head = req.query.replace('Bearer ','')
+const verifyJwt =  jwt.verify(head,process.env.JWT_ENV)
+console.log(verifyJwt)
+const finduser= await createUsers.findOne({_id:verifyJwt._id , 'token.tokenid':head})
+if(!finduser){
+    throw new Error()
+}
+req.user=finduser
+console.log(finduser)
+
     }
     console.log(headertoken)
     const verifyJwt =  jwt.verify(headertoken,process.env.JWT_ENV)
