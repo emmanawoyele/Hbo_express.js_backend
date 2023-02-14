b94fb94d780516ea5d77a1c63d9cf9ac1332a90a
emmanuel awoyele <emmanawoyele@gmail.com>
2023-02-14 09:50:52 +0000

    useCreateIndex removed

--- src/DataBase/mongoose.js
+++ src/DataBase/mongoose.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose")
 mongoose.connect((process.env.MONGODB_ENV),{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useFindAndModify:false,
-    useUnifiedTopology: true 
+
 })
 
 
